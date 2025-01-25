dfadc32d40521a5fdbc4639a23d85941cf336f3d
Daniel Bohlin <daniel.bohlin@live.se>
2025-01-26 00:02:12 +0100

    cleanup

--- Tharga.Fortnox/FortnoxConnectionService.cs
+++ Tharga.Fortnox/FortnoxConnectionService.cs
@@ -2,7 +2,7 @@
 using System.Text;
 using System.Text.Json;
 
-namespace Tharga.Fortnox.Internals;
+namespace Tharga.Fortnox;
 
 internal class FortnoxConnectionService : IFortnoxConnectionService
 {
